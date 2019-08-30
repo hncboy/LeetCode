fe525e5f2a500859df53c3bae18c20b89c0199b0
hncboy <619452863@qq.com>
2019-08-30 14:35:39 +0800

    MedianOfTwoSortedArrays

---
+++ src/com/hncboy/MedianOfTwoSortedArrays.java
@@ -0,0 +1,44 @@
+package com.hncboy;
+
+import java.util.Arrays;
+
+/**
+ * User: hncboy
+ * DateTime: 2019/8/30 14:03
+ * Description:
+ * <p>
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+ * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+ * You may assume nums1 and nums2 cannot be both empty.
+ * <p>
+ * Example 1:
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * The median is 2.0
+ * <p>
+ * Example 2:
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * The median is (2 + 3)/2 = 2.5
+ */
+public class MedianOfTwoSortedArrays {
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1, 2};
+        int[] nums2 = new int[]{3, 4};
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] nums3 = new int[nums1.length + nums2.length];
+        System.arraycopy(nums1, 0, nums3, 0, nums1.length);
+        System.arraycopy(nums2, 0, nums3, nums1.length, nums2.length);
+        Arrays.sort(nums3);
+
+        if (nums3.length % 2 == 0) {
+            return (nums3[nums3.length / 2 - 1] + nums3[nums3.length / 2]) / 2.0;
+        } else {
+            return nums3[nums3.length / 2];
+        }
+    }
+}
