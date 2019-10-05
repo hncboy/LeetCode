1824637e55aeecc7f0b8b4881d6dc7ba308d3def
hncboy <619452863@qq.com>
2019-10-05 10:56:17 +0800

    104.二叉树的最大深度

---
+++ src/com/hncboy/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,48 @@
+package com.hncboy;
+
+/**
+ * @author hncboy
+ * @date 2019/10/5 10:46
+ * @description 104.二叉树的最大深度
+ *
+ * 给定一个二叉树，找出其最大深度。
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最大深度 3 。
+ */
+public class MaximumDepthOfBinaryTree {
+
+    private static int max = 0;
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(3);
+        node.left = new TreeNode(9);
+        node.right = new TreeNode(20);
+        node.right.left = new TreeNode(15);
+        node.right.right = new TreeNode(7);
+        System.out.println(new MaximumDepthOfBinaryTree().maxDepth(node));
+    }
+
+    private int maxDepth(TreeNode root) {
+        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
