01ba63efd4e79d64251e4a241e9491eea10b1391
hncboy <619452863@qq.com>
2019-11-22 11:50:41 +0800

    560.和为K的子数组

---
+++ src/com/hncboy/SubarraySumEqualsK.java
@@ -0,0 +1,55 @@
+package com.hncboy;
+
+import java.util.HashMap;
+
+/**
+ * @author hncboy
+ * @date 2019/11/22 11:25
+ * @description 560.和为K的子数组
+ *
+ * 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+ *
+ * 示例 1 :
+ * 输入:nums = [1,1,1], k = 2
+ * 输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+ *
+ * 说明 :
+ * 数组的长度为 [1, 20,000]。
+ * 数组中元素的范围是 [-1000, 1000] ，且整数 k 的范围是 [-1e7, 1e7]。
+ */
+public class SubarraySumEqualsK {
+
+    public static void main(String[] args) {
+        SubarraySumEqualsK s = new SubarraySumEqualsK();
+        System.out.println(s.subarraySum2(new int[]{1, 2, 3, 2}, 3));
+    }
+
+    private int subarraySum2(int[] nums, int k) {
+        int count = 0;
+        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
+    private int subarraySum1(int[] nums, int k) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
