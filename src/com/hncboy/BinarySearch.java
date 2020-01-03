921540e013008130a9fc83e93da63b1a67a15d90
hncboy <619452863@qq.com>
2020-01-03 22:19:24 +0800

    add 704.二分查找

---
+++ src/com/hncboy/BinarySearch.java
@@ -0,0 +1,53 @@
+package com.hncboy;
+
+/**
+ * @author hncboy
+ * @date 2020/1/3 22:05
+ * @description 704.二分查找
+ *
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target，
+ * 写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ *
+ * 示例 1:
+ * 输入: nums = [-1,0,3,5,9,12], target = 9
+ * 输出: 4
+ * 解释: 9 出现在 nums 中并且下标为 4
+ *
+ * 示例 2:
+ * 输入: nums = [-1,0,3,5,9,12], target = 2
+ * 输出: -1
+ * 解释: 2 不存在 nums 中因此返回 -1
+ *
+ * 提示：
+ * 你可以假设 nums 中的所有元素是不重复的。
+ * n 将在 [1, 10000]之间。
+ * nums 的每个元素都将在 [-9999, 9999]之间。
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        BinarySearch b = new BinarySearch();
+        System.out.println(b.search(new int[]{-1, 0, 3, 5, 9, 12}, 9));
+        System.out.println(b.search(new int[]{-1, 0, 3, 5, 9, 12}, 2));
+    }
+
+    private int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
