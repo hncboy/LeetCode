1d47e1f292a507735916cc2925872f32cf9fdf1d
hncboy <619452863@qq.com>
2020-03-11 01:53:34 +0800

    update 1013.将数组分成和相等的三个部分

--- src/com/hncboy/PartitionArrayIntoThreePartsWithEqualSum.java
+++ src/com/hncboy/PartitionArrayIntoThreePartsWithEqualSum.java
@@ -4,26 +4,26 @@ package com.hncboy;
  * @author hncboy
  * @date 2019/12/9 10:24
  * @description 1013.将数组分成和相等的三个部分
- *
+ * <p>
  * 给定一个整数数组 A，只有我们可以将其划分为三个和相等的非空部分时才返回 true，否则返回 false。
  * 形式上，如果我们可以找出索引 i+1 < j 且满足 
  * (A[0] + A[1] + ... + A[i] == A[i+1] + A[i+2] + ... + A[j-1] == A[j] + A[j-1] + ... + A[A.length - 1]) 
  * 就可以将数组三等分。
- *
+ * <p>
  * 示例 1：
  * 输出：[0,2,1,-6,6,-7,9,1,2,0,1]
  * 输出：true
  * 解释：0 + 2 + 1 = -6 + 6 - 7 + 9 + 1 = 2 + 0 + 1
- *
+ * <p>
  * 示例 2：
  * 输入：[0,2,1,-6,6,7,9,-1,2,0,1]
  * 输出：false
- *
+ * <p>
  * 示例 3：
  * 输入：[3,3,6,5,-2,2,5,1,-9,4]
  * 输出：true
  * 解释：3 + 3 = 6 = 5 - 2 + 2 + 5 + 1 - 9 + 4
- *
+ * <p>
  * 提示：
  * 3 <= A.length <= 50000
  * -10000 <= A[i] <= 10000
@@ -35,6 +35,7 @@ public class PartitionArrayIntoThreePartsWithEqualSum {
         System.out.println(p.canThreePartsEqualSum(new int[]{0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1}));
         System.out.println(p.canThreePartsEqualSum(new int[]{0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1}));
         System.out.println(p.canThreePartsEqualSum(new int[]{3, 3, 6, 5, -2, 2, 5, 1, -9, 4}));
+        System.out.println(p.canThreePartsEqualSum(new int[]{10, -10, 10, -10, 10, -10, 10, -10}));
     }
 
     private boolean canThreePartsEqualSum(int[] A) {
@@ -55,6 +56,6 @@ public class PartitionArrayIntoThreePartsWithEqualSum {
                 count++;
             }
         }
-        return count == 3;
+        return count >= 3;
     }
 }
