45adf69633be08a502a3bf646598bb7064599af2
hncboy <619452863@qq.com>
2021-09-23 11:41:26 +0800

    :new: 剑指 Offer 14-I.剪绳子 :new: 343. 整数拆分

---
+++ src/com/hncboy/IntegerBreak.java
@@ -0,0 +1,43 @@
+package com.hncboy;
+
+/**
+ * @author hncboy
+ * @date 2021/9/23 11:37
+ * @description 343.整数拆分
+ *
+ * 给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
+ *
+ * 示例 1:
+ * 输入: 2
+ * 输出: 1
+ * 解释: 2 = 1 + 1, 1 × 1 = 1。
+ *
+ * 示例 2:
+ * 输入: 10
+ * 输出: 36
+ * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。
+ * 说明: 你可以假设 n 不小于 2 且不大于 58。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/integer-break
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IntegerBreak {
+
+    public static void main(String[] args) {
+        IntegerBreak integerBreak = new IntegerBreak();
+        assert integerBreak.integerBreak(2) == 1;
+        assert integerBreak.integerBreak(10) == 36;
+    }
+
+    private int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 2; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+}
---
+++ src/com/hncboy/swordreferstooffer/Question14_I.java
@@ -0,0 +1,40 @@
+package com.hncboy.swordreferstooffer;
+
+/**
+ * @author hncboy
+ * @date 2021/9/23 11:31
+ * @description 剑指 Offer 14-I.剪绳子
+ * 和这题一样 {@link com.hncboy.IntegerBreak}
+ * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m-1] 。
+ * 请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+ *
+ * 示例 1：
+ * 输入: 2
+ * 输出: 1
+ * 解释: 2 = 1 + 1, 1 × 1 = 1
+ *
+ * 示例 2:
+ * 输入: 10
+ * 输出: 36
+ * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36
+ *
+ * 提示：
+ * 2 <= n <= 58
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/jian-sheng-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Question14_I {
+
+    public int cuttingRope1(int n) {
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 2; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+}
