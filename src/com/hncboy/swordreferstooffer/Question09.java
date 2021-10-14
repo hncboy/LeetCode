f583f552e9ad8b34cbc1beb40e36c9542b639455
hncboy <619452863@qq.com>
2021-10-14 08:55:21 +0800

    :pencil2: 剑指 Offer 09 && 30

--- src/com/hncboy/swordreferstooffer/Question09.java
+++ src/com/hncboy/swordreferstooffer/Question09.java
@@ -5,10 +5,10 @@ import java.util.Stack;
 /**
  * @author hncboy
  * @date 2020/2/16 16:04
- * @description 面试题09.用两个栈实现队列
+ * @description 剑指 Offer 09.用两个栈实现队列
  *
  * 两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
- * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
  *
  * 示例 1：
  * 输入：
@@ -24,51 +24,50 @@ import java.util.Stack;
  *
  * 提示：
  * 1 <= values <= 10000
- * 最多会对 appendTail、deleteHead 进行 10000 次调用
+ * 最多会对 appendTail、deleteHead 进行 10000 次调用
  */
 public class Question09 {
 
-    private Stack<Integer> stack1;
-    private Stack<Integer> stack2;
-
     public static void main(String[] args) {
-        Question09 q = new Question09();
-        q.appendTail(3);
-        q.deleteHead();
-        q.deleteHead();
-        q.deleteHead();
+        CQueue c = new CQueue();
+        c.appendTail(3);
+        c.deleteHead();
+        c.deleteHead();
+        c.deleteHead();
     }
 
-    private Question09() {
-        stack1 = new Stack<>();
-        stack2 = new Stack<>();
-    }
+    private static class CQueue {
 
-    /**
-     * 添加整数到队列尾部
-     *
-     * @param value
-     */
-    private void appendTail(int value) {
-        // 往栈1中添加整数
-        stack1.push(value);
-    }
+        private final Stack<Integer> stack1;
+        private final Stack<Integer> stack2;
 
-    /**
-     * 删除队列头部的整数
-     *
-     * @return
-     */
-    private int deleteHead() {
-        // 如果栈2为空
-        if (stack2.empty()) {
-            // 遍历栈1，依次弹出整数并放入栈2，此时栈2的弹出顺序就是相当于从队列头部弹出
-            while (!stack1.empty()) {
-                stack2.push(stack1.pop());
-            }
+        public CQueue() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
         }
 
-        // 栈2还是为空的话就返回-1
-        return stack2.isEmpty() ? -1 : stack2.pop();
+        /**
+         * 添加整数到队列尾部
+         */
+        public void appendTail(int value) {
+            // 往栈1中添加整数
+            stack1.push(value);
+        }
+
+        /**
+         * 删除队列头部的整数
+         */
+        public int deleteHead() {
+            // 如果栈2为空
+            if (stack2.empty()) {
+                // 遍历栈1，依次弹出整数并放入栈2，此时栈2的弹出顺序就是相当于从队列头部弹出
+                while (!stack1.empty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
+
+            // 栈2还是为空的话就返回-1
+            return stack2.isEmpty() ? -1 : stack2.pop();
+        }
     }
 }
--- src/com/hncboy/swordreferstooffer/Question30.java
+++ src/com/hncboy/swordreferstooffer/Question30.java
@@ -5,10 +5,12 @@ import java.util.Stack;
 /**
  * @author hncboy
  * @date 2020/3/15 17:05
- * @description 面试题 30.包含min函数的栈
+ * @description 剑指 Offer 30.包含min函数的栈
+ * {@link com.hncboy.MinStack}
  *
  * 定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，
  * 调用 min、push 及 pop 的时间复杂度都是 O(1)。
+ *
  *
  * 示例:
  * MinStack minStack = new MinStack();
@@ -19,39 +21,51 @@ import java.util.Stack;
  * minStack.pop();
  * minStack.top();      --> 返回 0.
  * minStack.min();   --> 返回 -2.
- *  
+  *
  * 提示：
  * 各函数的调用总次数不超过 20000 次
+ *  
+ * 注意：本题与主站 155 题相同：https://leetcode-cn.com/problems/min-stack/
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/bao-han-minhan-shu-de-zhan-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Question30 {
 
-    private Stack<Integer> stack;
-    private int min;
+    private static class MinStack {
 
-    private Question30() {
-        stack = new Stack<>();
-        min = Integer.MAX_VALUE;
-    }
+        private final Stack<Integer> stack;
+        private int min;
 
-    private void push(int x) {
-        if (x <= min) {
-            stack.push(min);
-            min = x;
+        public MinStack() {
+            stack = new Stack<>();
+            min = Integer.MAX_VALUE;
         }
-        stack.push(x);
-    }
 
-    private void pop() {
-        if (stack.pop() == min) {
-            min = stack.pop();
+        public void push(int x) {
+            // 如果当前元素比最小值小，则先加入最小值，后面再加入当前元素
+            if (x <= min) {
+                stack.push(min);
+                min = x;
+            }
+            // 加入当前元素
+            stack.push(x);
         }
-    }
 
-    private int top() {
-        return stack.peek();
-    }
+        public void pop() {
+            // 如果弹出的是最小值，则最小值需要取再弹出一次的结果
+            if (stack.pop() == min) {
+                min = stack.pop();
+            }
+        }
 
-    private int min() {
-        return min;
+        public int top() {
+            return stack.peek();
+        }
+
+        public int min() {
+            return min;
+        }
     }
 }
