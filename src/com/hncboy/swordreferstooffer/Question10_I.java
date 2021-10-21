9b61e5b14e3f53e2c6bcd5bb897b0db91b6517c2
hncboy <619452863@qq.com>
2021-10-21 08:28:37 +0800

    :memo: :new: 剑指 Offer 63.股票的最大利润

--- src/com/hncboy/swordreferstooffer/Question10_I.java
+++ src/com/hncboy/swordreferstooffer/Question10_I.java
@@ -3,12 +3,13 @@ package com.hncboy.swordreferstooffer;
 /**
  * @author hncboy
  * @date 2020/2/16 17:10
- * @description 面试题10-I.斐波那契数列
- *
- * 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项。斐波那契数列的定义如下：
- * F(0) = 0,   F(1) = 1
+ * @description 剑指 Offer 10-I.斐波那契数列
+ * 
+ * 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
+ * F(0) = 0,   F(1) = 1
  * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
  * 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+ *
  * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
  *
  * 示例 1：
@@ -21,6 +22,10 @@ package com.hncboy.swordreferstooffer;
  *
  * 提示：
  * 0 <= n <= 100
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Question10_I {
 
@@ -35,7 +40,7 @@ public class Question10_I {
         for (int i = 2; i <= n; i++) {
             int c = a;
             a = b;
-            b = (b + c)%1000000007;
+            b = (b + c) % 1000000007;
         }
 
         return b;
--- src/com/hncboy/swordreferstooffer/Question10_II.java
+++ src/com/hncboy/swordreferstooffer/Question10_II.java
@@ -5,19 +5,29 @@ package com.hncboy.swordreferstooffer;
  * @date 2020/2/16 17:10
  * @description 面试题10-II.青蛙跳台阶问题
  *
- * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
  * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
  *
  * 示例 1：
  * 输入：n = 2
  * 输出：2
- *
+ * 
  * 示例 2：
  * 输入：n = 7
  * 输出：21
- *
+ * 
+ * 示例 3：
+ * 输入：n = 0
+ * 输出：1
+ * 
  * 提示：
  * 0 <= n <= 100
+ * 注意：本题与主站 70 题 {@link com.hncboy.ClimbingStairs}
+ * 相同：https://leetcode-cn.com/problems/climbing-stairs/
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Question10_II {
 
