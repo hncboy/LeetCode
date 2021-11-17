289255a373c2a6167595032f75bda249a4d908b5
hncboy <619452863@qq.com>
2021-11-17 09:52:44 +0800

    :memo: :new: 剑指 Offer II 030.插入、删除和随机访问都是 O(1) 的容器

--- src/com/hncboy/InsertDeleteGetrandomO1.java
+++ src/com/hncboy/InsertDeleteGetrandomO1.java
@@ -1,22 +1,50 @@
 package com.hncboy;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * @author hncboy
  * @date 2019/10/17 8:41
- * @description 380.常数时间插入、删除和获取随机元素
- * TODO
+ * @description 380.O(1) 时间插入、删除和获取随机元素
+ *
+ * 实现RandomizedSet 类：
+ * RandomizedSet() 初始化 RandomizedSet 对象
+ * bool insert(int val) 当元素 val 不存在时，向集合中插入该项，并返回 true ；否则，返回 false 。
+ * bool remove(int val) 当元素 val 存在时，从集合中移除该项，并返回 true ；否则，返回 false 。
+ * int getRandom() 随机返回现有集合中的一项（测试用例保证调用此方法时集合中至少存在一个元素）。每个元素应该有 相同的概率 被返回。
+ * 你必须实现类的所有函数，并满足每个函数的 平均 时间复杂度为 O(1) 。
+ *
+ * 示例：
+ * 输入
+ * ["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+ * [[], [1], [2], [2], [], [1], [2], []]
+ * 输出
+ * [null, true, false, true, 2, true, false, 2]
+ * 解释
+ * RandomizedSet randomizedSet = new RandomizedSet();
+ * randomizedSet.insert(1); // 向集合中插入 1 。返回 true 表示 1 被成功地插入。
+ * randomizedSet.remove(2); // 返回 false ，表示集合中不存在 2 。
+ * randomizedSet.insert(2); // 向集合中插入 2 。返回 true 。集合现在包含 [1,2] 。
+ * randomizedSet.getRandom(); // getRandom 应随机返回 1 或 2 。
+ * randomizedSet.remove(1); // 从集合中移除 1 ，返回 true 。集合现在包含 [2] 。
+ * randomizedSet.insert(2); // 2 已在集合中，所以返回 false 。
+ * randomizedSet.getRandom(); // 由于 2 是集合中唯一的数字，getRandom 总是返回 2 。
+ *  
+ *
+ * 提示：
+ * -231 <= val <= 231 - 1
+ * 最多调用 insert、remove 和 getRandom 函数 2 * 105 次
+ * 在调用 getRandom 方法时，数据结构中 至少存在一个 元素。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/insert-delete-getrandom-o1
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class InsertDeleteGetrandomO1 {
 
-    private List<Integer> result;
-
     public static void main(String[] args) {
         // 初始化一个空的集合。
-        InsertDeleteGetrandomO1 randomSet = new InsertDeleteGetrandomO1();
+        RandomizedSet randomSet = new RandomizedSet();
         // 向集合中插入 1 。返回 true 表示 1 被成功地插入。
         randomSet.insert(1);
         // 返回 false ，表示集合中不存在 2 。
@@ -33,42 +61,51 @@ public class InsertDeleteGetrandomO1 {
         randomSet.getRandom();
     }
 
-    /**
-     * Initialize your data structure here.
-     */
-    private InsertDeleteGetrandomO1() {
-        result = new ArrayList<>();
-    }
+    private static class RandomizedSet {
 
-    /**
-     * O(n)
-     * Inserts a value to the set. Returns true if the set did not already contain the specified element.
-     */
-    private boolean insert(int val) {
-        if (result.contains(val)) {
-            return false;
+        private final Map<Integer, Integer> dict;
+        private final List<Integer> list;
+        private final Random rand = new Random();
+
+        public RandomizedSet() {
+            dict = new HashMap<>();
+            list = new ArrayList<>();
         }
-        result.add(val);
-        return true;
-    }
 
-    /**
-     * O(n)
-     * Removes a value from the set. Returns true if the set contained the specified element.
-     */
-    private boolean remove(int val) {
-        if (result.contains(val)) {
-            return result.remove((Object)val);
+        public boolean insert(int val) {
+            if (dict.containsKey(val)) {
+                return false;
+            }
+
+            int index = list.size();
+            dict.put(val, index);
+            list.add(index, val);
+            return true;
         }
-        return false;
-    }
 
-    /**
-     * O(1)
-     * Get a random element from the set.
-     */
-    private int getRandom() {
-        int index = new Random().nextInt(result.size());
-        return result.get(index);
+        public boolean remove(int val) {
+            if (!dict.containsKey(val)) {
+                return false;
+            }
+
+            // 获取到最后一个元素的值
+            int lastElement = list.get(list.size() - 1);
+            // 获取到 val 对应下标
+            int index = dict.get(val);
+            // 将最后一个元素的下标移动被删除的位置
+            list.set(index, lastElement);
+            // 修改最后一个元素的对应的下标
+            dict.put(lastElement, index);
+            // list 列表长度 -1
+            list.remove(list.size() - 1);
+            // 移除该 val
+            dict.remove(val);
+
+            return true;
+        }
+
+        public int getRandom() {
+            return list.get(rand.nextInt(list.size()));
+        }
     }
 }
---
+++ src/com/hncboy/swordreferstoofferspecial/Question030.java
@@ -0,0 +1,90 @@
+package com.hncboy.swordreferstoofferspecial;
+
+import java.util.*;
+
+/**
+ * @author hncboy
+ * @date 2021/11/17 9:41
+ * @description 剑指 Offer II 030.插入、删除和随机访问都是 O(1) 的容器
+ * 
+ * 设计一个支持在平均 时间复杂度 O(1) 下，执行以下操作的数据结构：
+ * insert(val)：当元素 val 不存在时返回 true ，并向集合中插入该项，否则返回 false 。
+ * remove(val)：当元素 val 存在时返回 true ，并从集合中移除该项，否则返回 false 。
+ * getRandom：随机返回现有集合中的一项。每个元素应该有 相同的概率 被返回。
+ *
+ * 示例 :
+ * 输入: inputs = ["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+ * [[], [1], [2], [2], [], [1], [2], []]
+ * 输出: [null, true, false, true, 2, true, false, 2]
+ * 解释:
+ * RandomizedSet randomSet = new RandomizedSet();  // 初始化一个空的集合
+ * randomSet.insert(1); // 向集合中插入 1 ， 返回 true 表示 1 被成功地插入
+ * randomSet.remove(2); // 返回 false，表示集合中不存在 2
+ * randomSet.insert(2); // 向集合中插入 2 返回 true ，集合现在包含 [1,2]
+ * randomSet.getRandom(); // getRandom 应随机返回 1 或 2
+ * randomSet.remove(1); // 从集合中移除 1 返回 true 。集合现在包含 [2]
+ * randomSet.insert(2); // 2 已在集合中，所以返回 false
+ * randomSet.getRandom(); // 由于 2 是集合中唯一的数字，getRandom 总是返回 2
+ *  
+ * 提示：
+ * -231 <= val <= 231 - 1
+ * 最多进行 2 * 105 次 insert ， remove 和 getRandom 方法调用
+ * 当调用 getRandom 方法时，集合中至少有一个元素
+ *  
+ * 注意：本题与主站 380 题 {@link com.hncboy.InsertDeleteGetrandomO1}
+ * 相同：https://leetcode-cn.com/problems/insert-delete-getrandom-o1/
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/FortPu
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Question030 {
+
+    private static class RandomizedSet {
+
+        private final Map<Integer, Integer> dict;
+        private final List<Integer> list;
+        private final Random rand = new Random();
+
+        public RandomizedSet() {
+            dict = new HashMap<>();
+            list = new ArrayList<>();
+        }
+
+        public boolean insert(int val) {
+            if (dict.containsKey(val)) {
+                return false;
+            }
+
+            int index = list.size();
+            dict.put(val, index);
+            list.add(index, val);
+            return true;
+        }
+
+        public boolean remove(int val) {
+            if (!dict.containsKey(val)) {
+                return false;
+            }
+
+            // 获取到最后一个元素的值
+            int lastElement = list.get(list.size() - 1);
+            // 获取到 val 对应下标
+            int index = dict.get(val);
+            // 将最后一个元素的下标移动被删除的位置
+            list.set(index, lastElement);
+            // 修改最后一个元素的对应的下标
+            dict.put(lastElement, index);
+            // list 列表长度 -1
+            list.remove(list.size() - 1);
+            // 移除该 val
+            dict.remove(val);
+
+            return true;
+        }
+
+        public int getRandom() {
+            return list.get(rand.nextInt(list.size()));
+        }
+    }
+}
