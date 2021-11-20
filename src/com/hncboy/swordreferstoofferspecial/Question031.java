9f26c9ffadf3dee5d549b4181113b624677a63e0
hncboy <619452863@qq.com>
2021-11-20 17:11:20 +0800

    :memo: :new: 剑指 Offer II 031.最近最少使用缓存

--- src/com/hncboy/swordreferstoofferspecial/Question031.java
+++ src/com/hncboy/swordreferstoofferspecial/Question031.java
@@ -1,24 +1,20 @@
-package com.hncboy;
+package com.hncboy.swordreferstoofferspecial;
 
-import com.hncboy.swordreferstoofferspecial.Question031;
-
-import java.util.*;
+import java.util.HashMap;
 
 /**
  * @author hncboy
- * @date 2019/12/9 11:34
- * @description 146.LRU缓存机制
+ * @date 2021/11/20 16:39
+ * @description 剑指 Offer II 031.最近最少使用缓存
+ * 
+ * 运用所掌握的数据结构，设计和实现一个  LRU (Least Recently Used，最近最少使用) 缓存机制 。
  *
- * 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制 。
  * 实现 LRUCache 类：
- *
  * LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
  * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
  * void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。
  * 当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
- *
- * 进阶：你是否可以在 O(1) 时间复杂度内完成这两种操作？
- *
+ *  
  *
  * 示例：
  * 输入
@@ -45,31 +41,14 @@ import java.util.*;
  * 0 <= value <= 105
  * 最多调用 2 * 105 次 get 和 put
  *
+ * 进阶：是否可以在 O(1) 时间复杂度内完成这两种操作？
+ * 注意：本题与主站 146 题 {@link com.hncboy.LruCache} 相同：https://leetcode-cn.com/problems/lru-cache/
+ *
  * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/lru-cache
+ * 链接：https://leetcode-cn.com/problems/OrIXps
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class LruCache {
-
-    public static void main(String[] args) {
-        LRUCache cache = new LRUCache(2);
-        cache.put(1, 1);
-        cache.put(2, 2);
-        // 返回 1
-        System.out.println(cache.get(1));
-        // 该操作会使得密钥 2 作废
-        cache.put(3, 3);
-        // 返回 -1 (未找到)
-        System.out.println(cache.get(2));
-        // 该操作会使得密钥 1 作废
-        cache.put(4, 4);
-        // 返回 -1 (未找到)
-        System.out.println(cache.get(1));
-        // 返回 3
-        System.out.println(cache.get(3));
-        // 返回 4
-        System.out.println(cache.get(4));
-    }
+public class Question031 {
 
     private static class LRUCache {
 
