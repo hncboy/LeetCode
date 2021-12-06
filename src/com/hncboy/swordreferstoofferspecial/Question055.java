299a454e28f3955bfa7acf4f9d09da7a72f6897a
hncboy <619452863@qq.com>
2021-12-06 10:05:14 +0800

    :memo: :new: 剑指 Offer II 055.二叉搜索树迭代器

--- src/com/hncboy/swordreferstoofferspecial/Question055.java
+++ src/com/hncboy/swordreferstoofferspecial/Question055.java
@@ -1,29 +1,29 @@
-package com.hncboy;
+package com.hncboy.swordreferstoofferspecial;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author hncboy
- * @date 2021/12/6 9:43
- * @description 173.二叉搜索树迭代器
- *
+ * @date 2021/12/6 10:02
+ * @description 剑指 Offer II 055.二叉搜索树迭代器
+ * 
  * 实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+ *
  * BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root 会作为构造函数的一部分给出。
  * 指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
  * boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
  * int next()将指针向右移动，然后返回指针处的数字。
  * 注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
  *
- * 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
+ * 可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
  *
  * 示例：
  * 输入
- * ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
- * [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+ * inputs = ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+ * inputs = [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
  * 输出
  * [null, 3, 7, true, 9, true, 15, true, 20, false]
- *
  * 解释
  * BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
  * bSTIterator.next();    // 返回 3
@@ -43,13 +43,15 @@ import java.util.List;
  *
  * 进阶：
  * 你可以设计一个满足下述条件的解决方案吗？next() 和 hasNext() 操作均摊时间复杂度为 O(1) ，并使用 O(h) 内存。其中 h 是树的高度。
- * 通过次数 79,212 提交次数 98,355
+ *
+ * 注意：本题与主站 173 题 {@link com.hncboy.BinarySearchTreeIterator} 相同： https://leetcode-cn.com/problems/binary-search-tree-iterator/
+ * 通过次数 3,385 提交次数 3,842
  *
  * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/binary-search-tree-iterator
+ * 链接：https://leetcode-cn.com/problems/kTOapQ
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class BinarySearchTreeIterator {
+public class Question055 {
 
     private static class BSTIterator {
 
