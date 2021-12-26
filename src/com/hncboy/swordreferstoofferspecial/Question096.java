ca6888e9738eb708ee20ae28d76ab9b6533936e5
hncboy <619452863@qq.com>
2021-12-26 14:49:36 +0800

    :memo: :new: 剑指 Offer II 096.字符串交织

---
+++ src/com/hncboy/swordreferstoofferspecial/Question096.java
@@ -0,0 +1,70 @@
+package com.hncboy.swordreferstoofferspecial;
+
+/**
+ * @author hncboy
+ * @date 2021/12/26 14:47
+ * 剑指 Offer II 096.字符串交织
+ * 
+ * 给定三个字符串 s1、s2、s3，请判断 s3 能不能由 s1 和 s2 交织（交错） 组成。
+ * 两个字符串 s 和 t 交织 的定义与过程如下，其中每个字符串都会被分割成若干 非空 子字符串：
+ *
+ * s = s1 + s2 + ... + sn
+ * t = t1 + t2 + ... + tm
+ * |n - m| <= 1
+ * 交织 是 s1 + t1 + s2 + t2 + s3 + t3 + ... 或者 t1 + s1 + t2 + s2 + t3 + s3 + ...
+ * 提示：a + b 意味着字符串 a 和 b 连接。
+ *
+ * 示例 1：
+ * 输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+ * 输出：true
+ *
+ * 示例 2：
+ * 输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+ * 输出：false
+ *
+ * 示例 3：
+ * 输入：s1 = "", s2 = "", s3 = ""
+ * 输出：true
+ *
+ * 提示：
+ * 0 <= s1.length, s2.length <= 100
+ * 0 <= s3.length <= 200
+ * s1、s2、和 s3 都由小写英文字母组成
+ *
+ * 注意：本题与主站 97 题 {@link com.hncboy.InterleavingString} 相同： https://leetcode-cn.com/problems/interleaving-string/
+ * 通过次数 2,401 提交次数 4,887
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/IY6buf
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Question096 {
+
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
+        if (m + n != s3.length()) {
+            return false;
+        }
+
+        // dp[i][j] 表示 s1 的前 i 个字符和 s2 的前 j 个字符相加，是否交错成 s3 的前 i+j 个字符
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        for (int i = 1; i <= m && s1.charAt(i - 1) == s3.charAt(i - 1); i++) {
+            dp[i][0] = true;
+        }
+        for (int j = 1; j <= n && s2.charAt(j - 1) == s3.charAt(j - 1); j++) {
+            dp[0][j] = true;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1))
+                        || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+            }
+        }
+
+        return dp[m][n];
+    }
+}
