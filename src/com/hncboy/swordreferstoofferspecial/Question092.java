0ea90a533ad0cd2b32e64d54a40a77c3cf4704e0
hncboy <619452863@qq.com>
2022-01-14 09:34:57 +0800

    :memo: :new: 剑指 Offer II 092.翻转字符 :pencil: 926.将字符串翻转到单调递增

--- src/com/hncboy/swordreferstoofferspecial/Question092.java
+++ src/com/hncboy/swordreferstoofferspecial/Question092.java
@@ -1,42 +1,38 @@
-package com.hncboy;
+package com.hncboy.swordreferstoofferspecial;
 
 /**
  * @author hncboy
- * @date 2019/12/5 18:23
- * 926.将字符串翻转到单调递增
+ * @date 2022/1/14 9:00
+ * 剑指 Offer II 092.翻转字符
  *
- * 如果一个由 '0' 和 '1' 组成的字符串，是以一些 '0'（可能没有 '0'）后面跟着一些 '1'（也可能没有 '1'）的形式组成的，那么该字符串是单调递增的。
- * 我们给出一个由字符 '0' 和 '1' 组成的字符串 S，我们可以将任何 '0' 翻转为 '1' 或者将 '1' 翻转为 '0'。
- * 返回使 S 单调递增的最小翻转次数。
+ * 如果一个由 '0' 和 '1' 组成的字符串，是以一些 '0'（可能没有 '0'）后面跟着一些 '1'（也可能没有 '1'）的形式组成的，那么该字符串是 单调递增 的。
+ * 我们给出一个由字符 '0' 和 '1' 组成的字符串 s，我们可以将任何 '0' 翻转为 '1' 或者将 '1' 翻转为 '0'。
+ * 返回使 s 单调递增 的最小翻转次数。
  *
  * 示例 1：
- * 输入："00110"
+ * 输入：s = "00110"
  * 输出：1
  * 解释：我们翻转最后一位得到 00111.
  *
  * 示例 2：
- * 输入："010110"
+ * 输入：s = "010110"
  * 输出：2
  * 解释：我们翻转得到 011111，或者是 000111。
  *
  * 示例 3：
- * 输入："00011000"
+ * 输入：s = "00011000"
  * 输出：2
  * 解释：我们翻转得到 00000000。
  *
  * 提示：
- * 1 <= S.length <= 20000
- * S 中只包含字符 '0' 和 '1'
- * 通过次数 7,981 提交次数 14,867
+ * 1 <= s.length <= 20000
+ * s 中只包含字符 '0' 和 '1'
+ *
+ * 注意：本题与主站 926 题 {@link com.hncboy.FlipStringToMonotoneIncreasing}
+ * 相同： https://leetcode-cn.com/problems/flip-string-to-monotone-increasing/
+ * 通过次数 3,820 提交次数 5,644
  */
-public class FlipStringToMonotoneIncreasing {
-
-    public static void main(String[] args) {
-        FlipStringToMonotoneIncreasing f = new FlipStringToMonotoneIncreasing();
-        System.out.println(f.minFlipsMonoIncr("00110"));
-        System.out.println(f.minFlipsMonoIncr("010110"));
-        System.out.println(f.minFlipsMonoIncr("00011000"));
-    }
+public class Question092 {
 
     public int minFlipsMonoIncr(String s) {
         int n = s.length();
