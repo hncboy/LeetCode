b1c677d2028f3762de635fed17a513a4942640cc
hncboy <619452863@qq.com>
2022-01-19 08:39:22 +0800

    :memo: :new: 219.存在重复元素 II

--- src/com/hncboy/ContainsDuplicateII.java
+++ src/com/hncboy/ContainsDuplicateII.java
@@ -1,27 +1,33 @@
 package com.hncboy;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author hncboy
  * @date 2019/11/19 18:07
- * @description 219.存在重复元素 II
- * <p>
- * 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，
- * 使得 nums [i] = nums [j]，并且 i 和 j 的差的绝对值最大为 k。
- * <p>
- * 示例 1:
- * 输入: nums = [1,2,3,1], k = 3
- * 输出: true
- * <p>
- * 示例 2:
- * 输入: nums = [1,0,1,1], k = 1
- * 输出: true
- * <p>
- * 示例 3:
- * 输入: nums = [1,2,3,1,2,3], k = 2
- * 输出: false
+ * 219.存在重复元素 II
+ *
+ * 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。
+ * 如果存在，返回 true ；否则，返回 false 。
+ *
+ * 示例 1：
+ * 输入：nums = [1,2,3,1], k = 3
+ * 输出：true
+ *
+ * 示例 2：
+ * 输入：nums = [1,0,1,1], k = 1
+ * 输出：true
+ *
+ * 示例 3：
+ * 输入：nums = [1,2,3,1,2,3], k = 2
+ * 输出：false
+ *
+ * 提示：
+ * 1 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ * 0 <= k <= 105
+ * 通过次数 127,949 提交次数 299,982
  */
 public class ContainsDuplicateII {
 
@@ -33,18 +39,15 @@ public class ContainsDuplicateII {
     }
 
     private boolean containsNearbyDuplicate(int[] nums, int k) {
-        if (k == 35000) {
-            return false;
-        }
-        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
-            int index = map.getOrDefault(nums[i], -1);
-            map.put(nums[i], i);
-            if (index != -1) {
-                if (i - index <= k) {
-                    return true;
-                }
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
             }
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
         }
         return false;
     }
