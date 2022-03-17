78dc3b7fd7bdf41d0f1f7bad2c9a2ccb6f809a5c
hncboy <619452863@qq.com>
2022-03-17 08:29:08 +0800

    :memo: :new: 720.词典中最长的单词

--- README.md
+++ README.md
@@ -373,6 +373,7 @@
 | 714 | [买卖股票的最佳时机含手续费](https://github.com/hncboy/LeetCode/blob/master/src/com/hncboy/BestTimeToBuyAndSellStockWithTransactionFee.java) | 中等 |
 | [717](https://leetcode-cn.com/problems/1-bit-and-2-bit-characters/) | [1比特与2比特字符](https://github.com/hncboy/LeetCode/blob/master/src/com/hncboy/OneBitAndTwoBitCharacters.java) | 简单 |
 | 718 | [最长重复子数组](https://github.com/hncboy/LeetCode/blob/master/src/com/hncboy/MaximumLengthOfRepeatedSubarray.java) | 中等 |
+| [720](https://leetcode-cn.com/problems/longest-word-in-dictionary/) | [词典中最长的单词](https://github.com/hncboy/LeetCode/blob/master/src/com/hncboy/LongestWordInDictionary.java) | 简单 |
 | 724 | [寻找数组的中心索引](https://github.com/hncboy/LeetCode/blob/master/src/com/hncboy/FindPivotIndex.java) | 简单 |
 | 725 | [分隔链表](https://github.com/hncboy/LeetCode/blob/master/src/com/hncboy/SplitLinkedListInParts.java) | 中等 |
 | [729](https://leetcode-cn.com/problems/my-calendar-i/) | [我的日程安排表 I](https://github.com/hncboy/LeetCode/blob/master/src/com/hncboy/MyCalendarI.java) | 中等 |
---
+++ src/com/hncboy/LongestWordInDictionary.java
@@ -0,0 +1,88 @@
+package com.hncboy;
+
+/**
+ * @author hncboy
+ * @date 2022/3/17 8:17
+ * 720.词典中最长的单词
+ * 
+ * 给出一个字符串数组 words 组成的一本英语词典。返回 words 中最长的一个单词，该单词是由 words 词典中其他单词逐步添加一个字母组成。
+ * 若其中有多个可行的答案，则返回答案中字典序最小的单词。若无答案，则返回空字符串。
+ *
+ * 示例 1：
+ * 输入：words = ["w","wo","wor","worl", "world"]
+ * 输出："world"
+ * 解释： 单词"world"可由"w", "wo", "wor", 和 "worl"逐步添加一个字母组成。
+ *
+ * 示例 2：
+ * 输入：words = ["a", "banana", "app", "appl", "ap", "apply", "apple"]
+ * 输出："apple"
+ * 解释："apply" 和 "apple" 都能由词典中的单词组成。但是 "apple" 的字典序小于 "apply" 
+ *
+ * 提示：
+ * 1 <= words.length <= 1000
+ * 1 <= words[i].length <= 30
+ * 所有输入的字符串 words[i] 都只包含小写字母。
+ * 通过次数27,742提交次数56,414
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-word-in-dictionary
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LongestWordInDictionary {
+
+    public String longestWord(String[] words) {
+        // 遍历所有单词构建字典树
+        Trie trie = new Trie();
+        for (String word : words) {
+            trie.insert(word);
+        }
+
+        String longest = "";
+        for (String word : words) {
+            if (trie.search(word)) {
+                if (word.length() > longest.length() || (word.length() == longest.length() && word.compareTo(longest) < 0)) {
+                    longest = word;
+                }
+            }
+        }
+        return longest;
+    }
+}
+
+class Trie {
+
+    Trie[] children;
+    boolean isEnd;
+
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            // 判断该单词是否是 其他单词逐步添加一个字母组成的
+            if (node.children[index] == null || !node.children[index].isEnd) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return node != null && node.isEnd;
+    }
+}
